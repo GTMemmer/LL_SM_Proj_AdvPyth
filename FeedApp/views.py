0344d5b4fa73a3105e475ffacdbb8b1963636b8a
Gabriel Memmer <gabe_memmer1@baylor.edu>
2025-12-11 23:14:49 -0600

    spelling errors

--- FeedApp/views.py
+++ FeedApp/views.py
@@ -142,7 +142,7 @@ def friends(request):
 
     #this is to process all reqeusts received
     if request.method == 'POST' and request.POST.get("receive_requests"):
-        senders = request.POST.getlist("recieve_requests")
+        senders = request.POST.getlist("receive_requests")
         for sender in senders:
             #udpate the relationship model for the sender to status 'accpeted'
             Relationship.objects.filter(id=sender).update(status='accepted')
